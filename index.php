14ebe552d1af451ff64740a321ef0c15fbd1e738
ohotnikov.ivan <ohotnikov.ivan@e-queo.net>
2020-11-21 23:15:47 +0400

    fix

--- index.php
+++ index.php
@@ -7,4 +7,4 @@ use Lichi\Vk\Sdk\ApiProvider;
 $apiProvider = new ApiProvider("b4e7726db4e7726db4e7726dedb494c41abb4e7b4e7726debaa568e625ccf0b1a0b662a");
 $grabber = new Init($apiProvider);
 $postsAfterGrabbing = $grabber->getPostsFor(-192108616, 100);
-$postsAfterGrabbing = $grabber->getPostsFor(-188303783, 100);
+$postsAfterGrabbing = $grabber->getPostsFor(-183573646, 100);
