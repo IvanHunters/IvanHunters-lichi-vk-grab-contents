317fb2731fc8a7ca2767f935ff0323a4a3c60049
ohotnikov.ivan <ohotnikov.ivan@e-queo.net>
2020-11-21 19:42:46 +0400

    fix

--- index.php
+++ index.php
@@ -4,10 +4,6 @@ include "vendor/autoload.php";
 use Lichi\Grab\Post\Init;
 use Lichi\Vk\Sdk\ApiProvider;
 
-//$apiProvider = new ApiProvider(TOKEN);
-//$grabber = new Init($apiProvider);
-//$postsAfterGrabbing = $grabber->getPostsFor(OWNER_ID, COUNT);
-
-$apiProvider = new ApiProvider("b4e7726db4e7726db4e7726dedb494c41abb4e7b4e7726debaa568e625ccf0b1a0b662a");
+$apiProvider = new ApiProvider(TOKEN);
 $grabber = new Init($apiProvider);
-$postsAfterGrabbing = $grabber->getPostsFor(-62247825, 10);
+$postsAfterGrabbing = $grabber->getPostsFor(OWNER_ID, COUNT);
